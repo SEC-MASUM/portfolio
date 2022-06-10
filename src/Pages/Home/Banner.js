6694adc9f5f35c4b5785427b4b0dd60e1ca79459
SEC-MASUM <masum.jess42@gmail.com>
2022-06-10 12:53:45 +0600

    add icon

--- src/Pages/Home/Banner.js
+++ src/Pages/Home/Banner.js
@@ -1,6 +1,6 @@
 import React from "react";
 import photo from "../../assets/images/Profile-Picture.png";
-
+import {FaCloudDownloadAlt} from "react-icons/fa"
 const Banner = () => {
   return (
     <div className="container mx-auto">
@@ -8,7 +8,7 @@ const Banner = () => {
         <div className="flex-1 flex  items-center">
           <div className="px-6 py-12">
             <h4 className="text-3xl  font-medium text-green-500">Hello, I'm</h4>
-            <h1 className="text-6xl font-medium pt-4">Md. Masum Billah</h1>
+            <h1 className="text-6xl font-bold pt-4">Md. Masum Billah</h1>
             <p className="text-2xl  py-4 text-violet-500">
               {" "}
               Full Stack Web Developer
@@ -18,14 +18,17 @@ const Banner = () => {
               web application using React. Feel free to contact me for any React
               projects you might have in mind.
             </p> */}
-            <a
-              href="https://drive.google.com/file/d/1516clwC-JRrVQ5i8wDT2pJxcNMkBVuk9/view"
-              target="_blank"
-              className="btn btn-primary text-white"
-              rel="noreferrer"
-            >
-              Download Resume
-            </a>
+            <div className="flex items-center space-x-6">
+              <a
+                href="https://drive.google.com/file/d/1516clwC-JRrVQ5i8wDT2pJxcNMkBVuk9/view"
+                target="_blank"
+                className="btn btn-primary rounded-full text-white flex justify-between gap-2"
+                rel="noreferrer"
+              >
+                Download Resume{" "}
+                <FaCloudDownloadAlt className="h-6 w-6"></FaCloudDownloadAlt>
+              </a>
+            </div>
           </div>
         </div>
         <div className="flex-1 flex justify-center items-center">
