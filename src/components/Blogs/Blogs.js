134bd367298a226634155c9ab0c0bbd8ed572aef
SEC-MASUM <masum.jess42@gmail.com>
2022-06-10 12:54:44 +0600

    create Blogs page

---
+++ src/components/Blogs/Blogs.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Blogs = () => {
+  return (
+    <div className="h-screen flex justify-center items-center">
+      <h1 className="">Coming Soon</h1>
+    </div>
+  );
+};
+
+export default Blogs;
